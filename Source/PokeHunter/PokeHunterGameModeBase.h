87a1dca1dfe08433b1a1343e2e14fe46b62cb66e
Mgcllee <mgcllee@gmail.com>
2022-11-07 22:46:41 +0900

    [Server] AWS Default Settings
    AWS Default Settings

--- Source/PokeHunter/PokeHunterGameModeBase.h
+++ Source/PokeHunter/PokeHunterGameModeBase.h
@@ -2,6 +2,9 @@
 
 #pragma once
 
+// #include "aws/gamelift/internal/network/AuxProxyMessageSender.h"
+// #include "aws/gamelift/server/GameLiftServerAPI.h"
+#include "GameLiftServerSDK.h"
 #include "PokeHunter.h"
 #include "GameFramework/GameModeBase.h"
 #include "PokeHunterGameModeBase.generated.h"
@@ -9,13 +12,79 @@
 /**
  * 
  */
+
+USTRUCT()
+struct FStartGameSessionState
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+		bool Status;
+
+	FStartGameSessionState() {
+		Status = false;
+	}
+};
+
+USTRUCT()
+struct FUpdateGameSessionState
+{
+	GENERATED_BODY()
+
+	FUpdateGameSessionState() {
+
+	}
+};
+
+
+USTRUCT()
+struct FProcessTerminateState
+{
+	GENERATED_BODY()
+
+		UPROPERTY()
+		bool Status;
+
+	long TerminationTime;
+
+	FProcessTerminateState() {
+		Status = false;
+	}
+};
+
+USTRUCT()
+struct FHealthCheckState
+{
+	GENERATED_BODY()
+		bool Status;
+
+	FHealthCheckState() {
+		Status = false;
+	}
+};
+
 UCLASS()
 class POKEHUNTER_API APokeHunterGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 
-	APokeHunterGameModeBase();
-
 public:
+	APokeHunterGameModeBase();
 	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	UPROPERTY()
+		FStartGameSessionState StartGameSessionState;
+
+	UPROPERTY()
+		FUpdateGameSessionState UpdateGameSessionState;
+
+	UPROPERTY()
+		FProcessTerminateState ProcessTerminateState;
+
+	UPROPERTY()
+		FHealthCheckState HealthCheckState;
 };
--- Source/PokeHunterServer.Target.cs
+++ Source/PokeHunterServer.Target.cs
@@ -3,12 +3,15 @@
 using UnrealBuildTool;
 using System.Collections.Generic;
 
+[SupportedPlatforms(UnrealPlatformClass.Server)]
 public class PokeHunterServerTarget : TargetRules //이 행은 프로젝트 이름에 맞게 변경합니다.
 {
     public PokeHunterServerTarget(TargetInfo Target) : base(Target) //이 행은 프로젝트 이름에 맞게 변경합니다.
     {
         Type = TargetType.Server;
+
         DefaultBuildSettings = BuildSettingsVersion.V2;
+        
         ExtraModuleNames.Add("PokeHunter"); //이 행은 프로젝트 이름에 맞게 변경합니다.
     }
 }
